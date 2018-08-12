a09fc2ed018768600a8cb11848682e54c8341b26
Alex <alexanderlperez@gmail.com>
2018-08-12 19:08:33 -0400

    Updated gulpfile to support sass, ejs. Added base files for ejs, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
-const { src, dest } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
 const ejs = require('gulp-ejs');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const log = require('fancy-log');
+const rename = require('gulp-rename');
 
-// merge ejs
-// transpile sass
-
-function buildEJS() {
+function buildEJS(done) {
     return src('src/pages/*.ejs')
-        .pipe(ejs().on('error', log))
+        .pipe(ejs().on('error', (err) => { console.log(err.message); done() }))
         .pipe(dest('dist/'))
 }
 
 exports.ejs = buildEJS;
+
+function buildSASS() {
+    return src('src/theme/base.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(rename('style.css'))
+        .pipe(dest('dist/css/'))
+}
+
+exports.sass = buildSASS;
+
+exports.default = parallel(buildEJS, buildSASS);
+watch('src/pages/**/*', buildEJS);
+watch('src/theme/**/*', buildSASS);

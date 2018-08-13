2168910818a74172365ab39efd3958a046248f01
Alex <alexanderlperez@gmail.com>
2018-08-12 20:54:43 -0400

    Fixed EJS output extension (to html, from ejs). Now using gulp-changed to only compile changed files for EJS, SASS tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,31 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const log = require('fancy-log');
 const rename = require('gulp-rename');
+const bs = require('browser-sync').create();
+const changed = require('gulp-changed');
 
 function buildEJS(done) {
+    const DEST = 'dist/';
     return src('src/pages/*.ejs')
-        .pipe(ejs().on('error', (err) => { console.log(err.message); done() }))
-        .pipe(dest('dist/'))
+        .pipe(changed(DEST))
+        .pipe(ejs({}, {}, { ext: '.html' })
+            .on('error', (err) => { 
+                console.log(err.message); 
+                done();
+            }))
+        .pipe(dest(DEST))
 }
 
 exports.ejs = buildEJS;
 
 function buildSASS() {
-    return src('src/theme/base.scss')
+    const DEST = 'dist/css/';
+    return src('src/theme/*')
+        .pipe(changed(DEST))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(rename('style.css'))
-        .pipe(dest('dist/css/'))
+        .pipe(dest(DEST))
 }
 
 exports.sass = buildSASS;
